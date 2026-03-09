a90809eb970067219860334db68c3a9c385cddc7
Andres <andresloor@gmail.com>
2026-03-09 15:44:27 -0500

    Change security from settings to .env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jqi2ul-g0!ff%72e(328h72ue&8x7$3dn!u=jxb@)+lhrgh9v5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fallback-solo-para-dev')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
